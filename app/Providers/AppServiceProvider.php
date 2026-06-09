8922a922d4dc1c29882d2ffe2e6081a0be2dea00
Hariyogi <hariyogi.office@gmail.com>
2026-06-09 11:19:28 +0800

    fix: bug

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production' || env('APP_URL')) {
+            URL::forceScheme('https');
+        }
         Paginator::useTailwind();
     }
 }
